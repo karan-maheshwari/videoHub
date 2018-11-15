59404df303b718a535b1983dca5170f5b48143ea
karan-maheshwari <karanmaheshwari24@gmail.com>
2018-11-15 13:22:36 -0800

    TS File Testing
    TS File Testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'videoHub';
+  ngOnInit(): void {}
+  getKeyWords() {
+      const keywords = (document.getElementById('search-box') as HTMLInputElement).value;
+      const list_of_keywords = keywords.split(' ');
+      console.log(list_of_keywords);
+  }
 }
+
